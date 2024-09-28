12357a11596e84720e49f520d609ee829db8ca78
NadineBaldi <nanubaldi16@gmail.com>
2024-09-28 18:42:18 -0300

    KAN-72: Endpoint to get course info was added

--- src/api/subject.js
+++ src/api/subject.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { API_URL } from "../constants/environment";
 import { TOKEN, USER_ID } from "../constants/util";
-import { getCookie } from '../commons/helpers/cookies';
+import { getCookie } from "../commons/helpers/cookies";
 
 // Token
 const token = getCookie(TOKEN);
@@ -9,10 +9,14 @@ const userId = getCookie(USER_ID);
 
 const config = {
   headers: {
-    Authorization: `Bearer ${token}`
-  }
+    Authorization: `Bearer ${token}`,
+  },
 };
 
 export function getSubjectsByStudentId() {
-  return axios.get(`${API_URL}/subject/student/${userId}`, config)
+  return axios.get(`${API_URL}/subject/student/${userId}`, config);
+}
+
+export function getSubjectById(courseId) {
+  return axios.get(`${API_URL}/subject/find/${courseId}`, config);
 }
